b64e877817f36de48d235f77ba2018394e949cea
EpiCmaN213 <epicman213@gmail.com>
2015-04-04 16:08:47 +0200

    pushuje

--- BallSurvive-core/src/com/dpproject/ballsurvive/MyGame.java
+++ BallSurvive-core/src/com/dpproject/ballsurvive/MyGame.java
@@ -13,7 +13,7 @@ public class MyGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("badlogic.png");
 	}
 
 	@Override
