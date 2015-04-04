d36e745ddf3f7218e65022b11dacb4e966754ddc
EpiCmaN213 <epicman213@gmail.com>
2015-04-04 16:17:49 +0200

    haha

--- BallSurvive-core/src/com/dpproject/ballsurvive/MyGame.java
+++ BallSurvive-core/src/com/dpproject/ballsurvive/MyGame.java
@@ -7,13 +7,11 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGame extends ApplicationAdapter {
+	
 	SpriteBatch batch;
-	Texture img;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.png");
 	}
 
 	@Override
@@ -21,7 +19,6 @@ public class MyGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
 		batch.end();
 	}
 }
