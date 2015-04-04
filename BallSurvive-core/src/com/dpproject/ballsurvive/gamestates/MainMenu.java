863046586fb4436871aa980040d6e21d4112c15b
EpiCmaN213 <epicman213@gmail.com>
2015-04-04 17:02:10 +0200

    HEHE

--- BallSurvive-core/src/com/dpproject/ballsurvive/gamestates/MainMenu.java
+++ BallSurvive-core/src/com/dpproject/ballsurvive/gamestates/MainMenu.java
@@ -1,49 +1,47 @@
 package com.dpproject.ballsurvive.gamestates;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class MainMenu implements Screen {
 
+	private Stage stage = new Stage();
+	
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void render(float delta) {
-		// TODO Auto-generated method stub
-		
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+		stage.act();
+		stage.draw();
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void hide() {
-		// TODO Auto-generated method stub
-		
+		dispose();
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-		
+		stage.dispose();
 	}
 
 }
--- BallSurvive-desktop/src/com/dpproject/ballsurvive/desktop/DesktopLauncher.java
+++ BallSurvive-desktop/src/com/dpproject/ballsurvive/desktop/DesktopLauncher.java
@@ -8,11 +8,11 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Aim For the Target";
-		//config.useGL30 = true;
+		config.useGL30 = true;
 		config.useGL30 = false;
 		config.resizable = false;
-		config.height = 960;
-		config.width = 540;
+		config.height = MyGame.WIDTH;
+		config.width = MyGame.HEIGHT;
 		new LwjglApplication(new MyGame(), config);
 	}
 }
