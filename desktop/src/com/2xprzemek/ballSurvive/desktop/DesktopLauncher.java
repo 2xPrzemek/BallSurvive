6761b1574cb5e03822f3fcfd02aae3e9e3247c82
EpiCmaN213 <epicman213@gmail.com>
2015-04-04 15:58:22 +0200

    commit

---
+++ desktop/src/com/2xprzemek/ballSurvive/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.2xprzemek.ballSurvive.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.2xprzemek.ballSurvive.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
